1361ab1d6f60000904b60c36c3d80cfb857f5732
ezstuff <lhudson@breakpoint-labs.com>
2023-12-07 10:22:13 -0800

    Updated install script and packer bootstrap

--- installNvim.sh
+++ installNvim.sh
@@ -1,5 +1,14 @@
 #/bin/bash
-add-apt-repository ppa:neovim-ppa/unstable
-apt-get update
-apt-get install neovim
 
+# Installs the most recent dev branch of nvim (~0.10.x)
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+
+# Installs Packer plugin for nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# Moves nvim config to correct location
+mv ~/.config/nvim{,.bak}
+cp -r nvim ~/.config
