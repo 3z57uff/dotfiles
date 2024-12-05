ce7b8969dc941c715c69963e5b33aad4f1c9da30
ezstuff <lucas.m.hudson@gmail.com>
2024-12-05 16:18:39 -0500

    Updated nvim and hyprland conf

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -19,10 +19,16 @@ return {
         "neovim/nvim-lspconfig",
         lazy = false,
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
             local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({})
-            lspconfig.clangd.setup({})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities
+            })
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
@@ -33,3 +39,4 @@ return {
         end
     }
 }
+
